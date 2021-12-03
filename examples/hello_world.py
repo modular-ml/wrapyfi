ba4f2637e0184a7bbe0ec3969a050835030df14d
abawi <fares.abawi@uni-hamburg.de>
2021-12-03 19:49:15 +0100

    Updated publisher to avoid crashing on setting "should_wait"

---
+++ examples/hello_world.py
@@ -0,0 +1,19 @@
+from wrapify.connect.wrapper import MiddlewareCommunicator
+
+
+class HelloWorld(MiddlewareCommunicator):
+    @MiddlewareCommunicator.register("NativeObject", "HelloWorld", "/hello/my_message", carrier="", should_wait=True)
+    def send_message(self):
+        msg = input("Type your message: ")
+        obj = {"message": msg}
+        return obj,
+
+
+hello_world = HelloWorld()
+
+LISTEN = True
+hello_world.activate_communication("send_message", mode="listen"if LISTEN else "publish")
+
+while True:
+    my_message, = hello_world.send_message()
+    print(my_message["message"])
