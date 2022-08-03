d4b4f6846514d409fdde1b302b6a21a92ea711f6
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-08-03 13:45:12 +0200

    Implement ROS native object communicators

--- wrapify/listeners/ros.py
+++ wrapify/listeners/ros.py
@@ -1,29 +1,55 @@
+import contextlib
+import json
+import queue
 import rospy
+import std_msgs.msg
 
 from wrapify.connect.listeners import Listener, ListenerWatchDog, Listeners
+from wrapify.middlewares.ros import ROSMiddleware
+from wrapify.utils import JsonDecodeHook
+
+
+class ROSListener(Listener):
+
+    def __init__(self, name, in_port, carrier="", should_wait=False, queue_size=5):
+        super().__init__(name, in_port, carrier=carrier, should_wait=should_wait)
+        ROSMiddleware.activate()
+        self.queue_size = queue_size
 
 
 @Listeners.register("NativeObject", "ros")
-class ROSNativeObjectListener(Listener):
+class ROSNativeObjectListener(ROSListener):
 
-    def __init__(self, name, in_port, carrier="", should_wait=False, load_torch_device=None):
-        super().__init__(name, in_port, carrier=carrier, should_wait=should_wait)
-        # TODO: Initialise variables (e.g. subscriber = None)
+    def __init__(self, name, in_port, carrier="", should_wait=False, queue_size=5, load_torch_device=None):
+        super().__init__(name, in_port, carrier=carrier, should_wait=should_wait, queue_size=queue_size)
+        self._json_object_hook = JsonDecodeHook(torch_device=load_torch_device).object_hook
+        self._subscriber = None
+        self._queue = None
         ListenerWatchDog().add_listener(self)
 
     def establish(self):
-        # TODO: Create ROS subscriber
+        self._queue = queue.Queue(maxsize=0 if self.queue_size is None or self.queue_size <= 0 else self.queue_size)
+        self._subscriber = rospy.Subscriber(self.in_port, std_msgs.msg.String, callback=self._message_callback)
         self.established = True
 
     def listen(self):
         if not self.established:
             self.establish()
-        # TODO: Ctrl+C-able wait for data (or None) depending on self.should_wait
-        return None
+        try:
+            obj_str = self._queue.get(block=self.should_wait)
+            return json.loads(obj_str, object_hook=self._json_object_hook)
+        except queue.Empty:
+            return None
+
+    def _message_callback(self, data):
+        try:
+            self._queue.put(data.data, block=False)
+        except queue.Full:
+            print(f"Discarding data because listener queue is full: {self.in_port}")
 
 
 @Listeners.register("Image", "ros")
-class ROSImageListener(Listener):
+class ROSImageListener(ROSListener):
 
     def __init__(self, name, in_port, carrier="", should_wait=False, width=320, height=240, rgb=True):
         super().__init__(name, in_port, carrier=carrier, should_wait=should_wait)
@@ -35,7 +61,7 @@ class ROSImageListener(Listener):
 
 
 @Listeners.register("AudioChunk", "ros")
-class ROSAudioChunkListener(Listener):
+class ROSAudioChunkListener(ROSListener):
 
     def __init__(self, name, in_port, carrier="", should_wait=False, channels=1, rate=44100, chunk=-1):
         super().__init__(name, in_port, carrier=carrier, should_wait=should_wait)
@@ -47,7 +73,7 @@ class ROSAudioChunkListener(Listener):
 
 
 @Listeners.register("Properties", "ros")
-class ROSPropertiesListener(Listener):
+class ROSPropertiesListener(ROSListener):
 
     def __init__(self, name, in_port, **kwargs):
         super().__init__(name, in_port, **kwargs)
--- wrapify/publishers/ros.py
+++ wrapify/publishers/ros.py
@@ -1,28 +1,41 @@
+import json
 import rospy
+import std_msgs.msg
 
 from wrapify.connect.publishers import Publisher, Publishers, PublisherWatchDog
+from wrapify.middlewares.ros import ROSMiddleware
+from wrapify.utils import JsonEncoder
+
+
+class ROSPublisher(Publisher):
+
+    def __init__(self, name, out_port, carrier="", out_port_connect=None, queue_size=5, **kwargs):
+        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect, **kwargs)
+        ROSMiddleware.activate()
+        self.queue_size = queue_size
 
 
 @Publishers.register("NativeObject", "ros")
-class ROSNativeObjectPublisher(Publisher):
+class ROSNativeObjectPublisher(ROSPublisher):
 
-    def __init__(self, name, out_port, carrier="", out_port_connect=None, **kwargs):
-        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect)
-        # TODO: Initialise variables (e.g. publisher = None)
+    def __init__(self, name, out_port, carrier="", out_port_connect=None, queue_size=5, **kwargs):
+        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect, queue_size=queue_size, **kwargs)
+        self._publisher = None
         PublisherWatchDog().add_publisher(self)
 
     def establish(self):
-        # TODO: Create ROS publisher
+        self._publisher = rospy.Publisher(self.out_port, std_msgs.msg.String, queue_size=self.queue_size)
         self.established = True
 
     def publish(self, obj):
         if not self.established:
             self.establish()
-        # TODO: Publish obj to topic
+        obj_str = json.dumps(obj, cls=JsonEncoder)
+        self._publisher.publish(obj_str)
 
 
 @Publishers.register("Image", "ros")
-class ROSImagePublisher(Publisher):
+class ROSImagePublisher(ROSPublisher):
 
     def __init__(self, name, out_port, carrier="", out_port_connect=None, width=320, height=240, rgb=True, **kwargs):
         super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect)
@@ -34,7 +47,7 @@ class ROSImagePublisher(Publisher):
 
 
 @Publishers.register("AudioChunk", "ros")
-class ROSAudioChunkPublisher(Publisher):
+class ROSAudioChunkPublisher(ROSPublisher):
 
     def __init__(self, name, out_port, carrier="", out_port_connect=None, channels=1, rate=44100, chunk=-1, **kwargs):
         super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect)
@@ -46,7 +59,7 @@ class ROSAudioChunkPublisher(Publisher):
 
 
 @Publishers.register("Properties", "ros")
-class ROSPropertiesPublisher(Publisher):
+class ROSPropertiesPublisher(ROSPublisher):
 
     def __init__(self, name, out_port, **kwargs):
         super().__init__(name, out_port, **kwargs)
