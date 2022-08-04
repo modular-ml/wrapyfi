3106e63d86ae03e7b6eb96ae786c72e6d31575d8
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-08-04 15:58:04 +0200

    Image width/height tweaks

--- examples/icub_head.py
+++ examples/icub_head.py
@@ -108,12 +108,9 @@ class ICub(MiddlewareCommunicator, yarp.RFModule):
         self._curr_head = list(head)
         self._curr_eyes = list(eyes)
 
-    @MiddlewareCommunicator.register("Image", "yarp", "ICub", "$port_cam",
-                                     carrier="", width=320, height=240, rgb=True)
-    @MiddlewareCommunicator.register("Image", "yarp", "ICub", "$port_cam_left",
-                                     carrier="", width=320, height=240, rgb=True)
-    @MiddlewareCommunicator.register("Image", "yarp", "ICub", "$port_cam_right",
-                                     carrier="", width=320, height=240, rgb=True)
+    @MiddlewareCommunicator.register("Image", "yarp", "ICub", "$port_cam", carrier="", width=320, height=240, rgb=True)
+    @MiddlewareCommunicator.register("Image", "yarp", "ICub", "$port_cam_left", carrier="", width=320, height=240, rgb=True)
+    @MiddlewareCommunicator.register("Image", "yarp", "ICub", "$port_cam_right", carrier="", width=320, height=240, rgb=True)
     def receive_images(self, port_cam, port_cam_left, port_cam_right):
         external_cam, left_cam, right_cam = None, None, None
         return external_cam, left_cam, right_cam
