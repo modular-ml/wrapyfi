2577485d1f531e3073a62fb31626631df75c82f3
Philipp Allgeuer <philipp.allgeuer@uni-hamburg.de>
2022-08-12 11:33:12 +0200

    WIP on ROS image/audio communicators

--- wrapify/listeners/ros.py
+++ wrapify/listeners/ros.py
@@ -1,8 +1,9 @@
-import contextlib
 import json
 import queue
+import numpy as np
 import rospy
 import std_msgs.msg
+import sensor_msgs.msg
 
 from wrapify.connect.listeners import Listener, ListenerWatchDog, Listeners
 from wrapify.middlewares.ros import ROSMiddleware
@@ -23,8 +24,7 @@ class ROSNativeObjectListener(ROSListener):
     def __init__(self, name, in_port, carrier="", should_wait=True, queue_size=5, load_torch_device=None):
         super().__init__(name, in_port, carrier=carrier, should_wait=should_wait, queue_size=queue_size)
         self._json_object_hook = JsonDecodeHook(torch_device=load_torch_device).object_hook
-        self._subscriber = None
-        self._queue = None
+        self._subscriber = self._queue = None
         ListenerWatchDog().add_listener(self)
 
     def establish(self):
@@ -51,13 +51,44 @@ class ROSNativeObjectListener(ROSListener):
 @Listeners.register("Image", "ros")
 class ROSImageListener(ROSListener):
 
-    def __init__(self, name, in_port, carrier="", should_wait=True, width=320, height=240, rgb=True):
+    def __init__(self, name, in_port, carrier="", should_wait=True, width=-1, height=-1, rgb=True, fp=False):
         super().__init__(name, in_port, carrier=carrier, should_wait=should_wait)
         self.width = width
         self.height = height
         self.rgb = rgb
+        self.fp = fp
+        self._subscriber = self._type = self._queue = None
+        if self.fp:
+            self._encoding = '32FC3' if self.rgb else '32FC1'
+        else:
+            self._encoding = 'bgr8' if self.rgb else 'mono8'
         ListenerWatchDog().add_listener(self)
-        raise NotImplementedError
+
+    def establish(self):
+        self._queue = queue.Queue(maxsize=0 if self.queue_size is None or self.queue_size <= 0 else self.queue_size)
+        self._subscriber = rospy.Subscriber(self.in_port, sensor_msgs.msg.Image, callback=self._message_callback)
+        self._type = np.float32 if self.fp else np.uint8
+        self.established = True
+
+    def listen(self):
+        if not self.established:
+            self.establish()
+        try:
+            height, width, encoding, is_bigendian, data = self._queue.get(block=self.should_wait)
+            # TODO: Check height width against expected
+            # TODO: Check encoding against expected
+            # TODO: Check data length against expected
+            # TODO: Convert data to numpy array of required dtype/shape, respecting is_bigendian (required only for fp=true)
+            # TODO: Return numpy image
+            raise NotImplementedError
+        except queue.Empty:
+            return None
+
+    def _message_callback(self, data):
+        try:
+            self._queue.put((data.height, data.width, data.encoding, data.is_bigendian, data.data), block=False)
+        except queue.Full:
+            print(f"Discarding data because listener queue is full: {self.in_port}")
 
 
 @Listeners.register("AudioChunk", "ros")
@@ -68,8 +99,8 @@ class ROSAudioChunkListener(ROSListener):
         self.channels = channels
         self.rate = rate
         self.chunk = chunk
+        raise NotImplementedError  # TODO: Adapt ImageListener implementation for Audio chunk (chunk is expected length of array, channels is width)
         ListenerWatchDog().add_listener(self)
-        raise NotImplementedError
 
 
 @Listeners.register("Properties", "ros")
--- wrapify/publishers/ros.py
+++ wrapify/publishers/ros.py
@@ -1,7 +1,10 @@
+import sys
 import json
 import time
+import numpy as np
 import rospy
 import std_msgs.msg
+import sensor_msgs.msg
 
 from wrapify.connect.publishers import Publisher, Publishers, PublisherWatchDog
 from wrapify.middlewares.ros import ROSMiddleware
@@ -47,25 +50,78 @@ class ROSNativeObjectPublisher(ROSPublisher):
 @Publishers.register("Image", "ros")
 class ROSImagePublisher(ROSPublisher):
 
-    def __init__(self, name, out_port, carrier="", out_port_connect=None, width=320, height=240, rgb=True, **kwargs):
-        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect)
+    def __init__(self, name, out_port, carrier="", out_port_connect=None, width=-1, height=-1, rgb=True, fp=False, **kwargs):
+        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect, **kwargs)
         self.width = width
         self.height = height
         self.rgb = rgb
+        self.fp = fp
+        self._publisher = self._type = None
+        if self.fp:
+            self._encoding = '32FC3' if self.rgb else '32FC1'
+        else:
+            self._encoding = 'bgr8' if self.rgb else 'mono8'
         PublisherWatchDog().add_publisher(self)
-        raise NotImplementedError
+
+    def establish(self):
+        self._publisher = rospy.Publisher(self.out_port, sensor_msgs.msg.Image, queue_size=self.queue_size)
+        self._type = np.float32 if self.fp else np.uint8
+        self.await_connection(self._publisher)
+        self.established = True
+
+    def publish(self, img):
+        if not self.established:
+            self.establish()
+        if 0 < self.width != img.shape[1] or 0 < self.height != img.shape[0] or not ((img.ndim == 2 and not self.rgb) or (img.ndim == 3 and self.rgb and img.shape[2] == 3)):
+            raise ValueError("Incorrect image shape for publisher")
+        img = np.require(img, dtype=self._type, requirements='C')
+        msg = sensor_msgs.msg.Image()
+        msg.header.stamp = rospy.Time.now()
+        msg.height = img.shape[0]
+        msg.width = img.shape[1]
+        msg.encoding = self._encoding
+        msg.is_bigendian = img.dtype.byteorder == '>' or (img.dtype.byteorder == '=' and sys.byteorder == 'big')
+        msg.step = img.strides[0]
+        msg.data = img.tobytes()
+        self._publisher.publish(msg)
 
 
 @Publishers.register("AudioChunk", "ros")
 class ROSAudioChunkPublisher(ROSPublisher):
 
     def __init__(self, name, out_port, carrier="", out_port_connect=None, channels=1, rate=44100, chunk=-1, **kwargs):
-        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect)
+        super().__init__(name, out_port, carrier=carrier, out_port_connect=out_port_connect, **kwargs)
         self.channels = channels
         self.rate = rate
         self.chunk = chunk
+        self._publisher = None
         PublisherWatchDog().add_publisher(self)
-        raise NotImplementedError
+
+    def establish(self, **kwargs):
+        self._publisher = rospy.Publisher(self.out_port, sensor_msgs.msg.Image, queue_size=self.queue_size)
+        self.await_connection(self._publisher)
+        self.established = True
+
+    def publish(self, aud):
+        if not self.established:
+            self.establish()
+        aud, rate = aud
+        if rate != self.rate:
+            raise ValueError("Incorrect audio rate for publisher")
+        if aud is None:
+            return
+        if 0 < self.chunk != aud.shape[0] or aud.shape[1] != self.channels:
+            raise ValueError("Incorrect audio shape for publisher")
+        aud = np.require(aud, dtype=np.float32, requirements='C')
+        msg = sensor_msgs.msg.Image()
+        msg.header.stamp = rospy.Time.now()
+        msg.height = aud.shape[0]
+        msg.width = aud.shape[1]
+        msg.encoding = '32FC1'
+        msg.is_bigendian = aud.dtype.byteorder == '>' or (aud.dtype.byteorder == '=' and sys.byteorder == 'big')
+        msg.step = aud.strides[0]
+        msg.data = aud.tobytes()
+        self._publisher.publish(msg)
 
 
 @Publishers.register("Properties", "ros")
