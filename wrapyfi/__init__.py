7c62f79f6c6747a06de3cef0c54f5b2f41977058
abawi <abawi@informatik.uni-hamburg.de>
2022-12-01 17:51:21 +0100

    Updated init to avoid forced installation through setup.py

--- wrapyfi/__init__.py
+++ wrapyfi/__init__.py
@@ -1,11 +1,19 @@
+__version__ = "0.4.11"
+__url__ = "https://github.com/fabawi/wrapyfi/"
+name = "wrapyfi"
+
 try:
     from importlib import metadata
     __version__ = metadata.version(__name__)
     __url__ = metadata.metadata(__name__)["Home-page"]
 except ImportError:
-    import pkg_resources
-    __version__ = pkg_resources.require("wrapyfi")[0].version
-    __url__ = "https://github.com/fabawi/wrapyfi/"
+    try:
+        import pkg_resources
+        __version__ = pkg_resources.require(name)[0].version
+    except pkg_resources.DistributionNotFound:
+        pass
+except Exception:
+    pass
 
 from wrapyfi.utils import PluginRegistrar
 
