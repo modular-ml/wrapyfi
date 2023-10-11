4dcac52ee15354098e041bee3a8b02c8bda4024a
abawi <fares.abawi@uni-hamburg.de>
2023-10-11 04:38:04 +0200

    Documentation and typo fixes

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # uncomment following line, and rename index_API.rst to index.rst to get docs from code.
-#sphinx-apidoc -o source ../wrapyfi
+sphinx-apidoc -o source ../wrapyfi
 # uncomment following lines to compile example documentation
 sphinx-apidoc -o examples_docs ../examples
 # WARNING: Do not remove usage, since it is manually edited for now, given mdsplit cannot relink references
