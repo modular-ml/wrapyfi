9b6da4a59f3a566c8324e8d9affa1027cbcbfbaa
Fares Abawi <f.abawi@outlook.com>
2024-01-11 19:51:04 +0100

    Added dynamic root-link adjustment according to rank argument

--- docs/_extensions/link_modifier.py
+++ docs/_extensions/link_modifier.py
@@ -1,15 +1,15 @@
 from docutils import nodes
 from sphinx.transforms import SphinxTransform
+import re
 
 REPLACEMENTS = {
     'https://github.com/fabawi/wrapyfi/tree/main/wrapyfi_extensions/yarp/README.md':
         'yarp_install_lnk.html',
     'https://github.com/modular-ml/wrapyfi_ros2_interfaces/blob/master/README.md':
-        '../../ros2_interfaces_lnk.html',
+        'ros2_interfaces_lnk.html',
     'https://github.com/modular-ml/wrapyfi_ros_interfaces/blob/master/README.md':
-        '../../ros_interfaces_lnk.html'
-    }
-
+        'ros_interfaces_lnk.html'
+}
 
 class LinkModifier(SphinxTransform):
     default_priority = 999
@@ -17,10 +17,22 @@ class LinkModifier(SphinxTransform):
     def apply(self):
         for node in self.document.traverse(nodes.reference):
             uri = node.get('refuri', '')
-            for links in REPLACEMENTS.keys():
-                if links in uri:
-                    node['refuri'] = uri.replace(links, REPLACEMENTS[links])
+            for link in REPLACEMENTS.keys():
+                if link in uri:
+                    # Extract rank value
+                    match = re.search(r'\?rank=(-?\d+)', uri)
+                    if match:
+                        rank = int(match.group(1))
+                        if rank < 0:
+                            prefix = '../' * -rank
+                            new_uri = prefix + REPLACEMENTS[link]
+                        else:
+                            new_uri = REPLACEMENTS[link]
+                    else:
+                        # Default to rank -2 if no rank parameter
+                        new_uri = '../../' + REPLACEMENTS[link]
 
+                    node['refuri'] = uri.replace(link, new_uri)
 
 def setup(app):
     app.add_transform(LinkModifier)
